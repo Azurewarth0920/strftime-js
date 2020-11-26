0c44ebae3422c62d8e4db72b62e9d02648958fc3
pei.fan <fanpei920@gmail.com>
2020-11-26 23:48:59 +0900

    rewrite logic

--- src/lib.ts
+++ src/lib.ts
@@ -23,7 +23,7 @@ export function toFormatted(this: Date, format: string): string {
       this.getTime() -
       new Date(`${currentYear}/1/${weekBeginningDay}`).getTime()
 
-    return timeDiff > 0 ? Math.floor(timeDiff / 86400000 / 7) : 0
+    return timeDiff > 0 ? Math.floor(timeDiff / 86400000 / 7).toString() : '0'
   }
 
   type EnumToUnion<T extends string> = {
@@ -38,10 +38,7 @@ export function toFormatted(this: Date, format: string): string {
 
   const memo: PartialRecord<MemoKey, string> = {}
 
-  const memoize = (key: MemoKey, value: string) => {
-    memo[key] = value
-    return value
-  }
+  const memoize = (key: MemoKey, value: string) => (memo[key] = value)
 
   const enum Token {
     Year = 'y',
@@ -68,72 +65,145 @@ export function toFormatted(this: Date, format: string): string {
 
   const convert = (token: MemoKey) => {
     switch (token) {
-      // Double digits year.
       case Token.Year:
         return (
           memo[Token.Year] ||
           memoize(Token.Year, this.getFullYear().toString().substr(-2))
         )
-      // Full digits year.
+
       case Token.FullYear:
         return (
           memo[Token.FullYear] ||
           memoize(Token.FullYear, this.getFullYear().toString())
+        )
+
+      case Token.Month:
+        return (
+          memo[Token.Month] || memoize(Token.Month, this.getMonth().toString())
+        )
+
+      case Token.Date:
+        return (
+          memo[Token.Date] || memoize(Token.Date, this.getDate().toString())
+        )
+
+      case Token.MilitaryTime:
+        return (
+          memo[Token.MilitaryTime] ||
+          memoize(Token.MilitaryTime, this.getHours().toString())
+        )
+
+      case Token.ContinentalTime:
+        return (
+          memo[Token.ContinentalTime] ||
+          memoize(Token.ContinentalTime, (this.getHours() % 12).toString())
+        )
+
+      case Token.Minutes:
+        return (
+          memo[Token.Minutes] ||
+          memoize(Token.Minutes, this.getMinutes().toString())
+        )
+
+      case Token.Seconds:
+        return (
+          memo[Token.Seconds] ||
+          memoize(Token.Seconds, this.getSeconds().toString())
+        )
+
+      case Token.AbbrWeekday:
+        return (
+          memo[Token.AbbrWeekday] ||
+          memoize(Token.Seconds, getWeekDayName(this.getDay()).slice(0, 3))
+        )
+
+      case Token.FullWeekday:
+        return (
+          memo[Token.FullWeekday] ||
+          memoize(Token.FullWeekday, getWeekDayName(this.getDay()))
+        )
+
+      case Token.AbbrMonth:
+        return (
+          memo[Token.AbbrMonth] ||
+          memoize(
+            Token.AbbrMonth,
+            this.toLocaleString('default', { month: 'short' })
+          )
+        )
+
+      case Token.FullMonth:
+        return (
+          memo[Token.FullMonth] ||
+          memoize(
+            Token.FullMonth,
+            this.toLocaleString('default', { month: 'long' })
+          )
+        )
+
+      case Token.LocaleTime:
+        return (
+          memo[Token.LocaleTime] || memoize(Token.LocaleTime, this.toString())
+        )
+
+      case Token.DayInThisYear:
+        return (
+          memo[Token.DayInThisYear] ||
+          memoize(
+            Token.LocaleTime,
+            Math.ceil(
+              (this.getTime() -
+                new Date(`${this.getFullYear()}/1/1`).getTime()) /
+                86400000
+            ).toString()
+          )
+        )
+
+      case Token.Meridiem:
+        return (
+          memo[Token.Meridiem] ||
+          memoize(Token.LocaleTime, this.getHours() >= 12 ? 'PM' : 'AM')
+        )
+
+      case Token.WeekInYearFromSunday:
+        return (
+          memo[Token.WeekInYearFromSunday] ||
+          memoize(Token.WeekInYearFromSunday, toWeek())
+        )
+
+      case Token.WeekDay:
+        return (
+          memo[Token.WeekDay] ||
+          memoize(Token.WeekDay, this.getDay().toString())
+        )
+
+      case Token.WeekInYearFromMonday:
+        return (
+          memo[Token.WeekInYearFromMonday] ||
+          memoize(Token.WeekInYearFromMonday, toWeek(true))
+        )
+
+      case Token.LocaleDateString:
+        return (
+          memo[Token.LocaleDateString] ||
+          memoize(Token.LocaleDateString, this.toLocaleDateString())
+        )
+
+      case Token.LocaleTimeString:
+        return (
+          memo[Token.LocaleTimeString] ||
+          memoize(Token.LocaleTimeString, this.toLocaleTimeString())
         )
       default:
         return ''
     }
   }
 
-  format.replace(/[%]{1,2}[AaBbcdmHIjMSpUWwxXYy]/g, match => {
+  return format.replace(/[%]{1,2}[AaBbcdmHIjMSpUWwxXYy]/g, match => {
     if (match.length === 3) {
       return match.slice(1)
     } else {
       return convert(match.slice(1) as MemoKey)
     }
   })
-
-  fastReplace('%m', this.getMonth())
-  // Date.
-  fastReplace('%d', this.getDate())
-  // Military time.
-  fastReplace('%H', this.getHours())
-  // Continental time.
-  fastReplace('%I', this.getHours() % 12)
-  // Minutes.
-  fastReplace('%M', this.getMinutes())
-  // Seconds.
-  fastReplace('%S', this.getSeconds())
-  // Weekday abbreviations.
-  fastReplace('%a', getWeekDayName(this.getDay()).slice(0, 3))
-  // Weekday.
-  fastReplace('%A', getWeekDayName(this.getDay()))
-  // Month abbreviations.
-  fastReplace('%b', this.toLocaleString('default', { month: 'short' }))
-  // Month.
-  fastReplace('%B', this.toLocaleString('default', { month: 'long' }))
-  // Locale Time.
-  fastReplace('%c', this.toString())
-  // Day in this year.
-  fastReplace(
-    '%j',
-    Math.ceil(
-      (this.getTime() - new Date(`${this.getFullYear()}/1/1`).getTime()) /
-        86400000
-    )
-  )
-  // Noon or afternoon.
-  fastReplace('%p', this.getHours() >= 12 ? 'PM' : 'AM')
-  // Week in this year(from Sunday).
-  fastReplace('%U', toWeek())
-  // Week day in digit.
-  fastReplace('%w', this.getDay())
-  // xth week in this year(from Monday).
-  fastReplace('%W', toWeek(true))
-  // locale date string.
-  fastReplace('%x', this.toLocaleDateString())
-  // locale time string.
-  fastReplace('%X', this.toLocaleTimeString())
-
-  return format
 }
